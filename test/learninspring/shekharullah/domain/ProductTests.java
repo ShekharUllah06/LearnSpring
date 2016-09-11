95b8fc256ada1cb6624fecb4a538851ae02d70b8
Shekhar Ullah <ullah.shekhar@gmail.com>
2016-09-11 16:04:41 +0600

    Test Case is added for Product class

---
+++ test/learninspring/shekharullah/domain/ProductTests.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package learninspring.shekharullah.domain;
+
+import junit.framework.TestCase;
+import learnspring.shekharullah.domain.Product;
+
+/**
+ * @author Shekhar Ullah
+ *
+ */
+public class ProductTests extends TestCase {
+	private Product product;
+
+	protected void setUp() throws Exception {
+		product = new Product();
+	}
+
+	public void testSetAndGetDescription() {
+		String testDescription = "aDescription";
+		assertNull(product.getDescription());
+		product.setDescription(testDescription);
+		assertEquals(testDescription, product.getDescription());
+	}
+
+	public void testSetAndGetPrice() {
+		double testPrice = 100.00;
+		assertEquals(0, 0, 0);
+		product.setPrice(testPrice);
+		assertEquals(testPrice, product.getPrice(), 0);
+	}
+}
