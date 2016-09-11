65bef90f0f750a5caa7e0838ae1d4c2330f7b71a
Shekhar Ullah <ullah.shekhar@gmail.com>
2016-09-11 16:16:18 +0600

    SimpleProductManager class is added but not implement all methods

---
+++ src/learninspring/shekharullah/service/SimpleProductManager.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package learninspring.shekharullah.service;
+
+import java.util.List;
+
+import learnspring.shekharullah.domain.Product;
+
+/**
+ * @author Shekhar Ullah
+ *
+ */
+public class SimpleProductManager implements ProductManager {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see learninspring.shekharullah.service.ProductManager#increasePrice(int)
+	 */
+	@Override
+	public void increasePrice(int percentage) {
+		throw new UnsupportedOperationException();
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see learninspring.shekharullah.service.ProductManager#getProducts()
+	 */
+	@Override
+	public List<Product> getProducts() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void setProducts(List<Product> products) {
+		throw new UnsupportedOperationException();
+	}
+
+}
