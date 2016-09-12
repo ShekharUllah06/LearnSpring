8af60c4493f157d5d3c8aa72d8740dd5cce14b27
Shekhar Ullah <ullah.shekhar@gmail.com>
2016-09-12 11:29:53 +0600

    InventoryControllerTests is added

--- src/learnspring/shekharullah/web/InventoryController.java
+++ src/learnspring/shekharullah/web/InventoryController.java
@@ -2,6 +2,8 @@ package learnspring.shekharullah.web;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -17,17 +19,31 @@ import learnspring.shekharullah.service.ProductManager;
 public class InventoryController implements Controller {
 
 	protected final Log logger = LogFactory.getLog(getClass());
-	
+
 	private ProductManager productManager;
 
-    @Override
+	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+			throws ServletException, IOException {
 
-        String now = (new Date()).toString();
-        logger.info("Returning hello view with " + now);
+		String now = (new Date()).toString();
+		logger.info("Returning hello view with " + now);
 
-        return new ModelAndView("hello", "now", now);
-    }
+		Map<String, Object> myModel = new HashMap<String, Object>();
+		myModel.put("now", now);
+		myModel.put("products", this.productManager.getProducts());
+
+		return new ModelAndView("hello", "model", myModel);
+	}
+
+	/**
+	 * @param productManager
+	 */
+	public void setProductManager(ProductManager productManager) {
+		this.productManager=productManager;
+		
+	}
+
+	
 
 }
--- test/learnspring/shekharullah/web/InventoryControllerTests.java
+++ test/learnspring/shekharullah/web/InventoryControllerTests.java
@@ -1,17 +1,24 @@
 package learnspring.shekharullah.web;
 
+import java.util.Map;
+
 import org.springframework.web.servlet.ModelAndView;
+
 import junit.framework.TestCase;
+import learnspring.shekharullah.service.ProductManager;
+import learnspring.shekharullah.service.SimpleProductManager;
 
 
 public class InventoryControllerTests extends TestCase {
 
     public void testHandleRequestView() throws Exception{		
     	InventoryController controller = new InventoryController();
+        controller.setProductManager((ProductManager)(new SimpleProductManager()));
         ModelAndView modelAndView = controller.handleRequest(null, null);
-        assertEquals("jsp/hello.jsp", modelAndView.getViewName());
+        assertEquals("hello", modelAndView.getViewName());
         assertNotNull(modelAndView.getModel());
-        String nowValue = (String) modelAndView.getModel().get("now");
+        Map modelMap = (Map) modelAndView.getModel().get("model");
+        String nowValue = (String) modelMap.get("now");
         assertNotNull(nowValue);
     }
 }
